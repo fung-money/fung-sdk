9ba0f519791b706675be07fec0e68d7eac934086
Ashwini Agarwal <ashwini@fung.money>
2023-03-15 14:53:01 +0530

    feat: checkout: add event listener

--- packages/checkout/src/config.ts
+++ packages/checkout/src/config.ts
@@ -3,7 +3,7 @@ export const CHECKOUT_ENDPOINT_PROD = 'https://app.fung.money';
 
 export const CHECKOUT_EVENTS = [
   'checkout:loaded',
-  'checkout:expired',
+  'checkout:invalid',
   'checkout:completed',
   'checkout:failed',
 ];
