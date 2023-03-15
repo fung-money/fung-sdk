b45b5f48bed0a10c48cf32cf724858ba1546c389
Ashwini Agarwal <104421470+ashwini-fung@users.noreply.github.com>
2023-03-15 14:54:38 +0530

    feat: checkout: add event listener (#2)
    * feat: checkout: add event listener
    
    * feat: checkout: add event listener

--- packages/checkout/src/config.ts
+++ packages/checkout/src/config.ts
@@ -1,4 +1,9 @@
 export const CHECKOUT_ENDPOINT_SBX = 'https://sbx.fung.money';
 export const CHECKOUT_ENDPOINT_PROD = 'https://app.fung.money';
 
-export const CHECKOUT_LOADED_EVENT = 'CHECKOUT_LOADED';
+export const CHECKOUT_EVENTS = [
+  'checkout:loaded',
+  'checkout:invalid',
+  'checkout:completed',
+  'checkout:failed',
+];
