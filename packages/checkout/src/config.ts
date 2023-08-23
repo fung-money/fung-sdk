38e28d8fb624574389c580c211ef52953d71c092
Konstantin Indjov <cinjoff@gmail.com>
2023-08-23 16:11:52 +0300

    lint

--- packages/checkout/src/config.ts
+++ packages/checkout/src/config.ts
@@ -1,10 +1,10 @@
-export const CHECKOUT_ENDPOINT_SBX = "https://sbx.fung.money";
-export const CHECKOUT_ENDPOINT_DEV = "https://dev.fung.money";
-export const CHECKOUT_ENDPOINT_PROD = "https://app.fung.money";
+export const CHECKOUT_ENDPOINT_SBX = 'https://sbx.fung.money';
+export const CHECKOUT_ENDPOINT_DEV = 'https://dev.fung.money';
+export const CHECKOUT_ENDPOINT_PROD = 'https://app.fung.money';
 
 export enum CheckoutEvent {
-  Loaded = "checkout:loaded",
-  Invalid = "checkout:invalid",
-  Completed = "checkout:completed",
-  Failed = "checkout:failed",
+  Loaded = 'checkout:loaded',
+  Invalid = 'checkout:invalid',
+  Completed = 'checkout:completed',
+  Failed = 'checkout:failed',
 }
